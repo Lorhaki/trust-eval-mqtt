1532bc1afd02326e47a64f2895e6843b036061b8
Adrien <adrien.balssel@gmail.com>
2024-01-22 19:32:47 +0100

    Navigation Bar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -11,6 +11,50 @@ class HomePage extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
             title: Text("TrustEval"),
+            backgroundColor: Colors.red,
+          ),
+          body: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    "Voici l'application TrustEval.",
+                        style: TextStyle(
+                          fontSize: 24,
+                  ),
+                ),
+                Text(
+                  "Une application permettant de creer des evenements et de les approuver",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            )
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: Colors.white),
+                label: 'Actualités',
+                backgroundColor: Colors.red,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search, color: Colors.white),
+                label: 'Recherche',
+                backgroundColor: Colors.red,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person, color: Colors.white),
+                label: 'Profil',
+                backgroundColor: Colors.red,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add, color: Colors.white),
+                label: 'Ajouter un evenements',
+                backgroundColor: Colors.red,
+              ),
+            ],
           ),
         )
     );
