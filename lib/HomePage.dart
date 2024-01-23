ea98664bcf9ad3f582fa30b96dbb690971dfc0c1
Adrien <adrien.balssel@gmail.com>
2024-01-23 16:07:30 +0100

    Switch entre les pages

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -7,55 +7,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("TrustEval"),
-            backgroundColor: Colors.red,
-          ),
-          body: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                    "Voici l'application TrustEval.",
-                        style: TextStyle(
-                          fontSize: 24,
-                  ),
-                ),
-                Text(
-                  "Une application permettant de creer des evenements et de les approuver",
-                  style: TextStyle(
-                    fontSize: 12,
-                  ),
-                ),
-              ],
-            )
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home, color: Colors.white),
-                label: 'Actualités',
-                backgroundColor: Colors.red,
+    return const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Voici l'application TrustEval.",
+              style: TextStyle(
+                fontSize: 24,
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search, color: Colors.white),
-                label: 'Recherche',
-                backgroundColor: Colors.red,
+            ),
+            Text(
+              "Une application permettant de creer des evenements et de les approuver",
+              style: TextStyle(
+                fontSize: 12,
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.white),
-                label: 'Profil',
-                backgroundColor: Colors.red,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add, color: Colors.white),
-                label: 'Ajouter un evenements',
-                backgroundColor: Colors.red,
-              ),
-            ],
-          ),
+            ),
+          ],
         )
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'MonProfil.dart';
 import 'HomePage.dart';
 
 void main() {
@@ -7,13 +7,70 @@ void main() {
   
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return HomePage();
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int _currentIndex = 0;
+
+  setCurrentIndex(int index){
+    setState(() {
+      _currentIndex = index;
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text("TrustEval"),
+        ),
+        body: [
+          HomePage(),
+          MyProfile()
+        ][_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setCurrentIndex(index),
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Colors.grey,
+
+          elevation: 40,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Actualités',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Recherche',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Ajouter un evenements',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Ajouter un evenements',
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
+
+
 
