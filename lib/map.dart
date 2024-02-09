61f4167de04eee1c852963e069b809b4920c761b
AdrienBlass <adrien.blassel@gmail.com>
2024-02-09 16:59:57 +0100

    map

--- lib/map.dart
+++ lib/map.dart
@@ -1,71 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-//import 'package:flutter_map/flutter_map.dart';
 
 class Mymap extends StatefulWidget {
   const Mymap({super.key});
 
   @override
   State<Mymap> createState() => _MyMarkerMap();
-
 }
-class _MyMarkerMap extends State<Mymap>{
 
-  final users = [
-    {
-      "x": "51.0",
-      "y": "-0.12",
-      "nom":"Loic"
-    },
-    {
-      "x": "-0.12",
-      "y": "51.0",
-      "nom":"Loic"
-    },
-    {
-      "x": "50.32",
-      "y": "3.51",
-      "nom":"UPHF"
-    }
-  ]
-  ;
+class _MyMarkerMap extends State<Mymap> {
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
+    return Center(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("Events").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+
+          if (!snapshot.hasData) {
+            return Text("Aucun evenement disponible");
+          }
+
+          List<dynamic> events = [];
+          snapshot.data!.docs.forEach((element) {
+            events.add(element);
+          });
+
+          return FlutterMap(
             options: MapOptions(
-              center: LatLng( 51.0 , -0.12),
+              center: LatLng(51.0, -0.12),
               zoom: 12,
             ),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example.app',
-            ),
-            MarkerLayer(
-              markers:users.map((user) {
-                final latitude = double.parse(user['x']!);
-                final longitude = double.parse(user['y']!);
-
-                return Marker(
-                      width: 45.0,
-                      height:45.0,
-                      point: LatLng(latitude, longitude),
-                      builder: (context) => Container(
-                      child: IconButton(
-                      icon: const Icon(Icons.location_on),
-                      color: Colors.red,
-                      onPressed: () {
-                            },
-                       ),
-              )
-                );
-              }).toList(),
+            children: [
+              TileLayer(
+                urlTemplate:
+                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
               ),
-    ],
+              MarkerLayer(
+                markers: events.map((event) {
+                  final latitude = double.parse(event['localisationX']!);
+                  final longitude = double.parse(event['localisationY']!);
+
+                  return Marker(
+                    width: 45.0,
+                    height: 45.0,
+                    point: LatLng(latitude, longitude),
+                    builder: (context) => Container(
+                      child: IconButton(
+                        icon: const Icon(Icons.location_on),
+                        color: Colors.red,
+                        onPressed: () => {}, // Add placeholder function
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
-
-
-
